bb20b42d9d210e45c31fd528be48639fe7496a93
WangZhenyu <847315251@qq.com>
2018-12-22 21:26:50 +0800

    t_user_wallet增加可用余额与冻结余额字段

--- documents/zywork_app_data_test.sql
+++ documents/zywork_app_data_test.sql
@@ -389,6 +389,8 @@ CREATE TABLE `t_user_wallet` (
   `id` bigint(20) NOT NULL COMMENT '钱包编号',
   `pay_password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '支付密码',
   `rmb_balance` bigint(20) DEFAULT '0' COMMENT '人民币余额',
+  `usable_rmb_balance` bigint(20) DEFAULT '0' COMMENT '可用余额',
+  `frozen_rmb_balance` bigint(20) DEFAULT '0' COMMENT '冻结余额',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   `is_active` tinyint(4) DEFAULT NULL COMMENT '是否激活',
@@ -399,7 +401,7 @@ CREATE TABLE `t_user_wallet` (
 -- Records of t_user_wallet
 -- ----------------------------
 BEGIN;
-INSERT INTO `t_user_wallet` VALUES (31, '$2a$10$f8mTBz3qQdyrKalljtffkOr3vbWfGA8CyinLKbeHOCG/UmDLr1B5y', 0, NULL, NULL, NULL);
+INSERT INTO `t_user_wallet` VALUES (31, '$2a$10$f8mTBz3qQdyrKalljtffkOr3vbWfGA8CyinLKbeHOCG/UmDLr1B5y', 0, 0, 0, NULL, NULL, NULL);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
