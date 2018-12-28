c3f968465362fa898909e979041e583ae7739e27
WangZhenyu <847315251@qq.com>
2018-12-28 09:46:28 +0800

    在documents目录中增加分销设计数据库表、存储过程和相关查询SQL

---
+++ documents/N级分销设计/fengxiao.sql
@@ -0,0 +1,114 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 80013
+ Source Host           : localhost:3306
+ Source Schema         : fengxiao
+
+ Target Server Type    : MySQL
+ Target Server Version : 80013
+ File Encoding         : 65001
+
+ Date: 21/12/2018 12:28:54
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户编号',
+  `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '手机号',
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '用户邮箱',
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '登录密码',
+  `salt` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '加密盐值',
+  `version` int(11) DEFAULT '1' COMMENT '版本号',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `is_active` tinyint(4) DEFAULT '0' COMMENT '是否激活',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='用户基本信息表';
+
+-- ----------------------------
+-- Table structure for t_user_hierarchy
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_hierarchy`;
+CREATE TABLE `t_user_hierarchy` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '关系编号',
+  `ancestor_id` int(11) DEFAULT NULL COMMENT '祖先编号',
+  `user_id` int(11) DEFAULT NULL COMMENT '用户编号',
+  `user_level` int(11) DEFAULT NULL COMMENT '用户级别',
+  `version` int(11) DEFAULT '1' COMMENT '版本号',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `is_active` tinyint(4) DEFAULT '0' COMMENT '是否激活',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='用户关系表';
+
+-- ----------------------------
+-- Table structure for t_user_path
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_path`;
+CREATE TABLE `t_user_path` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '路径编号',
+  `user_path` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT '用户路径',
+  `version` int(11) DEFAULT '1' COMMENT '版本号',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `is_active` tinyint(4) DEFAULT '0' COMMENT '是否激活',
+  PRIMARY KEY (`id`),
+  FULLTEXT KEY `idx_user_path` (`user_path`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='用户关系路径表';
+
+-- ----------------------------
+-- Procedure structure for invite_user
+-- ----------------------------
+CREATE DEFINER=`root`@`localhost` PROCEDURE `invite_user`(in pid int, in uid int)
+BEGIN
+    -- 如果uid等于pid，表示某个用户自己成为顶级经销商
+	declare ancestorId int;
+	declare num int default 0;
+	declare userLevel int default 2;
+	declare currentTime datetime default CURRENT_TIMESTAMP;
+	declare userPath text;
+	declare hasError integer default 0;
+	-- 根据直接父id获取其所有祖先id，祖先id按倒序排列，方便得出用户id与祖先id间是多少级关系
+	declare ancestorList cursor for
+	select t_user_hierarchy.ancestor_id from t_user_hierarchy where t_user_hierarchy.user_id = pid
+	order by t_user_hierarchy.ancestor_id desc;
+	declare continue handler for sqlexception set hasError = 1;
+	declare continue handler for not found set num = 1;
+	start transaction;
+		if uid != pid then
+			open ancestorList;
+				fetch ancestorList into ancestorId;
+				while num != 1 do
+					insert into t_user_hierarchy (ancestor_id, user_id, user_level, create_time) values(ancestorId, uid, userLevel, currentTime);
+					-- 祖先id每循环一次，等级加1
+					set userLevel = userLevel + 1;
+					fetch ancestorList into ancestorId;
+				end while;
+			close ancestorList;
+		end if;
+		-- 自己与自己的关系，user_level为1
+		insert into t_user_hierarchy (ancestor_id, user_id, user_level) values(uid, uid, 1);
+		-- 保存用户层级路径
+		if uid != pid then
+			select t_user_path.user_path into userPath from t_user_path where t_user_path.user_path like concat('%', pid);
+			insert into t_user_path (user_path, create_time) values(concat(userPath, '/', uid), currentTime);
+		else
+			insert into t_user_path (user_path, create_time) values(uid, currentTime);
+		end if;
+	if hasError = 1 then
+		rollback;
+	else
+		commit;
+  end if;
+END
+
+SET FOREIGN_KEY_CHECKS = 1;
