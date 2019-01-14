b209a77b273e15999748d0530ee94e44291425ad
WangZhenyu <847315251@qq.com>
2019-01-14 22:22:57 +0800

    更新数据表

--- documents/zywork_app_data_test.sql
+++ documents/zywork_app_data_test.sql
@@ -188,7 +188,6 @@ CREATE TABLE `t_goods_attribute` (
   `attr_code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '属性代码',
   `attr_type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '数据类型',
   `attr_length` int(11) NOT NULL COMMENT '数据长度',
-  `attr_order` int(11) NOT NULL COMMENT '属性排序',
   `attr_display` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否前端显示',
   `version` int(11) DEFAULT '1' COMMENT '版本号',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -254,6 +253,7 @@ CREATE TABLE `t_goods_category_attribute` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '类目属性编号',
   `category_id` bigint(20) NOT NULL COMMENT '商品类目编号',
   `attr_id` bigint(20) NOT NULL COMMENT '属性编号',
+  `attr_order` int(11) NOT NULL COMMENT '属性排序',
   `version` int(11) DEFAULT '1' COMMENT '版本号',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
@@ -520,6 +520,7 @@ DROP TABLE IF EXISTS `t_goods_sku`;
 CREATE TABLE `t_goods_sku` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'SKU编号',
   `goods_id` bigint(20) NOT NULL COMMENT '商品编号',
+  `shelf_status` tinyint(4) NOT NULL COMMENT '上架状态',
   `version` int(11) DEFAULT '1' COMMENT '版本号',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
