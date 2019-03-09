5ee9490e9c7db2943fd0ab45743d49d61d6d16a7
WangZhenyu <847315251@qq.com>
2019-03-09 10:40:46 +0800

    更新XssFilter过滤器，可配置不需要过滤的url

--- src/main/java/top/zywork/filter/XssFilter.java
+++ src/main/java/top/zywork/filter/XssFilter.java
@@ -2,6 +2,9 @@ package top.zywork.filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import top.zywork.common.StringUtils;
 import top.zywork.wrapper.XssHttpServletRequestWrapper;
 
 import javax.servlet.*;
@@ -17,25 +20,33 @@ import java.io.IOException;
  * @author 王振宇
  * @version 1.0
  */
+@Component(value = "myXssFilter")
 @WebFilter(filterName="xssFilter", urlPatterns="/*")
 public class XssFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(XssFilter.class);
 
+    @Value("${xssFilter.ignoreUrls}")
+    private String[] ignoreUrls;
+
     @Override
     public void init(FilterConfig config) throws ServletException {
-        logger.info("XSSFilter init......");
+        logger.info("XSSFilter过滤器启动......");
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        XssHttpServletRequestWrapper xssHttpServletRequestWrapper = new XssHttpServletRequestWrapper((HttpServletRequest)request);
-        chain.doFilter(xssHttpServletRequestWrapper, response);
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        if (StringUtils.isInArray(ignoreUrls, httpServletRequest.getRequestURI())) {
+            chain.doFilter(request, response);
+        } else {
+            chain.doFilter(new XssHttpServletRequestWrapper(httpServletRequest), response);
+        }
     }
 
     @Override
     public void destroy() {
-        logger.info("XSSFilter destroy......");
+        logger.info("XSSFilter过滤器销毁......");
     }
 
 }
